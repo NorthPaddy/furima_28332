f0cacc626eab3891738ea1686be432808b717806
uuking0818 <believe.you.trust.you@gmail.com>
2020-08-17 15:59:33 +0900

    ユーザー管理機能の実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,18 @@
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  with_options presence: true do
+    validates :nickname
+    validates :email,
+      uniqueness: true,
+      format: { with: /\A\S+@\S+\.\S+\z/, message: "Email can't be blank"}
+    validates :password,
+      format: { with: /\A[a-z\d]{6,}+\z/i}
+    validates :family_name, :first_name,
+      format: { with: /\A[ぁ-んァ-ン一-龥]/, message: "name can't be blank"}
+    validates :family_name_kana, :first_name_kana,
+      format: { with: /\A[ぁ-んァ-ン一-龥]/, message: "First name kana can't be blank"}
+    validates :birthday
+  end
 end
