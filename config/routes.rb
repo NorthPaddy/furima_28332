ba9a84e52fc644714b2cfaa3623e31bb1c5d068b
uuking0818 <believe.you.trust.you@gmail.com>
2020-08-25 11:55:10 +0900

    order,addressテーブル作成

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,6 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "home#index"
   resources :items, only: [:new, :create, :show, :destroy, :edit, :update]
+  resources :orders, only: [:create]
+  resources :address
 end
