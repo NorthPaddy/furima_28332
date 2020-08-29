a16b8493eeca6e260bea3b56effa3c7aee99e85e
uuking0818 <believe.you.trust.you@gmail.com>
2020-08-29 19:03:12 +0900

    修正３

--- spec/models/order_address_spec.rb
+++ spec/models/order_address_spec.rb
@@ -54,5 +54,10 @@ RSpec.describe OrderAddress, type: :model do
       @order_address.valid?
       expect(@order_address.errors.full_messages).to include('Telephone number is out of setting range')
     end
+    it 'tokenが空だと保存できない'do
+      @order_address.token = nil
+      @order_address.valid?
+      expect(@order_address.errors.full_messages).to include("Token can't be blank")
+    end
   end
 end
